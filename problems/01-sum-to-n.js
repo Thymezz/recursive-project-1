eef52648c09f91b4a08aa4e3433eb043c4e04d0e
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2023-10-17 21:44:53 -0700

    sum to n solved

--- problems/01-sum-to-n.js
+++ problems/01-sum-to-n.js
@@ -11,11 +11,32 @@ sumToN(9)  // returns 45
 sumToN(-8)  // returns null
 ***********************************************************************/
 
+/*
+  input: integer
+  output: integer or null
+
+  base case: n === 0
+  recursive step: n - 1
+
+*/
+
 
 function sumToN(n) {
-  // Your code here 
+
+  if (n < 0) return null;
+
+  if (n === 0) return 0;
+  console.log(n);
+  let sum = n + sumToN(n - 1);
+  console.log(n, sum -n, sum)
+  return sum;
+
 }
 
+sumToN(5) // returns 15
+sumToN(1)  // returns 1
+sumToN(9)  // returns 45
+sumToN(-8)  // returns null
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
